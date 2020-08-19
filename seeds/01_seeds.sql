a18d95afa1b945fc77704199934716bef72511a6
Jenny Citherlet <jennycith@gmail.com>
2020-08-19 21:58:45 +0000

    Fix post code name

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -5,7 +5,7 @@ INSERT INTO users (id, name, email, password)
 
 
 
- INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, postal_code, active)
+ INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
  VALUES (1,'Rustic Cabin','description','https://i.pinimg.com/originals/c3/23/28/c323289fe61dca809fa6b7e59993b792.png','https://i.pinimg.com/originals/52/21/62/522162479a89f64392bfcecccbadced0.jpg', 150, 3, 2,1, 'Canada', 'Hidden Point','Bralorne','British Columbia','Y8J4D7',true),
  (2,'Beach Dream','description','https://i.pinimg.com/originals/6b/e3/59/6be3593da2dc87d1be9f2f0c1b4e407b.jpg','https://i.pinimg.com/originals/85/10/2b/85102b968f66df49810c3a55f4caaeb2.jpg', 500, 5, 7,5, 'United States', '4 Sun Street','San Diego','California','85684',true),
  (1,'Whistler Cabin','description','https://i.pinimg.com/originals/4e/63/58/4e6358124983c8b015445b25b196afdd.jpg','https://i.pinimg.com/originals/d1/a7/eb/d1a7ebda395fa105fadc4d01c831857e.jpg', 480, 3, 3,2, 'Canada', '184 Nancy Green Drive','Whistler','British Columbia','V8E0W9', true);
