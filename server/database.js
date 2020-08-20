e8735c52b6fabf1a89b2501f3d1dccd61c17bac3
Jenny Citherlet <jennycith@gmail.com>
2020-08-20 21:36:34 +0000

    Add query to select all reservation data for a logged in user

--- server/database.js
+++ server/database.js
@@ -82,7 +82,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit]
+
+  return pool.query(`
+  SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN reservations ON properties.id = property_id
+  JOIN property_reviews ON reservations.id = reservation_id
+  JOIN users on property_reviews.guest_id = users.id
+  WHERE users.id = $1
+  GROUP BY reservations.start_date, properties.id
+  ORDER BY start_date
+  LIMIT $2
+  `, values)
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 exports.getAllReservations = getAllReservations;
 
